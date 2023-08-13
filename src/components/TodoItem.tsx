2d5d61d05287d24f0f83188d9fd46acf270345dd
Kai Takami <kaitakami@protonmail.com>
2023-08-12 22:16:54 -0600

    feat: make component client

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,24 @@
+"use client"
+
 import React from 'react'
+import { useState } from 'react'
 
 type TodoItemProps = {
   id: string;
   title: string;
   complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => Promise<number>
 }
 
-const TodoItem = ({ id, title, complete }: TodoItemProps) => {
+const TodoItem = ({ id, title, complete, toggleTodo }: TodoItemProps) => {
+  const [animeId, setAnimeId] = useState<number>(1)
   return (
     <li className='flex gap-1 items-center'>
-      <input id={id} type='checkbox' className='cursor-pointer peer' />
+      {animeId}
+      <input id={id} type='checkbox' className='cursor-pointer peer' defaultChecked={complete} onChange={e => {
+        const res = toggleTodo(id, e.target.checked)
+        res.then(animeId => setAnimeId(animeId))
+      }} />
       <label htmlFor={id} className='cursor-pointer peer-checked:line-through peer-checked:text-zinc-500'>
         {title}
       </label>
